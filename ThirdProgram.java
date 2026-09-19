74d2ea12dd64d1e56f176a8fb5cacbf32b6a1177
VikramGanjhu <vikram.202104117@gmail.com>
2026-09-19 14:55:24 +0530

    Add files via upload

---
+++ ThirdProgram.java
@@ -0,0 +1,14 @@
+public class ThirdProgram {
+    public static void main(String[] args) {
+        int X=10,Y=20,C=30;
+        if(X==Y){
+        System.out.println("Both are same.");
+        }
+        else{
+            System.out.println("X and Y are not same.");
+        }
+        System.out.println(X);
+        System.out.println(Y);
+    }
+    
+}
